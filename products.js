241a41c0f99a5b5de8573e6a5574d14932599ea8
sethcrisel22 <sethcrisel.22@gmail.com>
2026-05-07 14:18:47 -0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,17 @@
+// The "Brain" - One link per price point
+const stripeLinks = {
+  "25": "https://buy.stripe.com/cNi6oHf784L2goUdSgeZ202"
+};
+
+// The Inventory List
+const products = [
+  { 
+    id: "MDSH1", 
+    name: "Electric Horizon Pocket Tee", 
+    price: "25", 
+    size: "LG", 
+    cat: "tshirts", 
+    img: "MDSH1.jpg", 
+    status: "available" 
+  }
+];
