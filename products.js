70fb590d7fe792f46a9d8c6c775483850b22fcee
sethcrisel22 <sethcrisel.22@gmail.com>
2026-05-08 09:20:55 -0700

    Update products.js

--- products.js
+++ products.js
@@ -1,57 +1,58 @@
-// --- STRIPE LINKS ---
+// --- CATEGORY PAY LINKS ---
 const stripeLinks = {
-  "10": "https://buy.stripe.com/7sY5kD4sufpGc8E6pOeZ201",
-  "25": "https://buy.stripe.com/cNi6oHf784L2goUdSgeZ202",
-  "45": "https://buy.stripe.com/eVq7sLaQSdhyc8E8xWeZ203",
-  "55": "https://buy.stripe.com/eVqcN58IK91ifkQ15ueZ207",
-  "75": "https://buy.stripe.com/4gM9AT7EG4L22y49C0eZ204",
-  "300": "https://buy.stripe.com/14A8wP3oqa5ma0w15ueZ205"
+  "shirt": "https://buy.stripe.com/cNi6oHf784L2goUdSgeZ202",
+  "pants": "https://buy.stripe.com/eVq7sLaQSdhyc8E8xWeZ203",
+  "socks": "https://buy.stripe.com/7sY5kD4sufpGc8E6pOeZ201",
+  "tapestry": "https://buy.stripe.com/4gM9AT7EG4L22y49C0eZ204",
+  "bedding": "https://buy.stripe.com/14A8wP3oqa5ma0w15ueZ205",
+  "hoodie": "https://buy.stripe.com/eVqcN58IK91ifkQ15ueZ207"
 };
 
-// --- PRODUCT INVENTORY ---
+// --- INVENTORY DATA ---
+// NOTE: Make sure the 'img' filename matches your GitHub file EXACTLY (.jpeg vs .jpg)
 const products = [
   { 
-    id: "SH-01", 
-    name: "Classic Spiral Tee", 
+    id: "SH-001", 
+    name: "Diagonal Rainbow Sweep", 
     price: "25", 
     size: "MED", 
-    cat: "shirt", // Matches HTML data-filter="shirt"
+    cat: "shirt", 
     img: "shirt.jpeg", 
     status: "available" 
   },
   { 
-    id: "PA-01", 
-    name: "Galaxy Joggers", 
+    id: "PA-001", 
+    name: "Pastel Galaxy Joggers", 
     price: "45", 
     size: "LG", 
-    cat: "pants", // Matches HTML data-filter="pants"
+    cat: "pants", 
     img: "pants.jpeg", 
     status: "available" 
   },
   { 
-    id: "SK-01", 
-    name: "Watercolor Knee Socks", 
-    price: "10", 
-    size: "OS", 
-    cat: "socks", // Matches HTML data-filter="socks"
-    img: "socks.jpeg", 
-    status: "available" 
-  },
-  { 
-    id: "TAP-01", 
-    name: "Mandala Wall Hanging", 
+    id: "TAP-001", 
+    name: "Electric Peace Mandala", 
     price: "75", 
-    size: "XL", 
-    cat: "tapestry", // Matches HTML data-filter="tapestry"
+    size: "Giant", 
+    cat: "tapestry", 
     img: "tapestry.jpeg", 
     status: "available" 
   },
   { 
-    id: "BD-01", 
+    id: "SK-001", 
+    name: "Watercolor Knee Highs", 
+    price: "10", 
+    size: "OS", 
+    cat: "socks", 
+    img: "socks.jpeg", 
+    status: "available" 
+  },
+  { 
+    id: "BD-001", 
     name: "Sunburst Bedspread Set", 
     price: "300", 
     size: "Queen", 
-    cat: "bedding", // Matches HTML data-filter="bedding"
+    cat: "bedding", 
     img: "bedspread.jpeg", 
     status: "available" 
   }
