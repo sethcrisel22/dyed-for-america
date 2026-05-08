1e5c79930c92ca7565401c24e8095f7fcf843fce
sethcrisel22 <sethcrisel.22@gmail.com>
2026-05-08 09:36:29 -0700

    Update products.js

--- products.js
+++ products.js
@@ -1,4 +1,3 @@
-// --- CATEGORY PAY LINKS ---
 const stripeLinks = {
   "shirt": "https://buy.stripe.com/cNi6oHf784L2goUdSgeZ202",
   "pants": "https://buy.stripe.com/eVq7sLaQSdhyc8E8xWeZ203",
@@ -8,51 +7,10 @@ const stripeLinks = {
   "hoodie": "https://buy.stripe.com/eVqcN58IK91ifkQ15ueZ207"
 };
 
-// --- INVENTORY DATA ---
 const products = [
-  { 
-    id: "SH-001", 
-    name: "Diagonal Rainbow Sweep", 
-    price: "25", 
-    size: "MED", 
-    cat: "shirt", 
-    img: "shirt.jpeg", 
-    status: "available" 
-  },
-  { 
-    id: "PA-001", 
-    name: "Pastel Galaxy Joggers", 
-    price: "45", 
-    size: "LG", 
-    cat: "pants", 
-    img: "pants.jpeg", 
-    status: "available" 
-  },
-  { 
-    id: "TAP-001", 
-    name: "Electric Peace Mandala", 
-    price: "75", 
-    size: "Giant", 
-    cat: "tapestry", 
-    img: "tapestry.jpeg", 
-    status: "available" 
-  },
-  { 
-    id: "SK-001", 
-    name: "Watercolor Knee Highs", 
-    price: "10", 
-    size: "OS", 
-    cat: "socks", 
-    img: "socks.jpeg", 
-    status: "available" 
-  },
-  { 
-    id: "BD-001", 
-    name: "Sunburst Bedspread Set", 
-    price: "300", 
-    size: "Queen", 
-    cat: "bedding", 
-    img: "bedspread.jpeg", 
-    status: "available" 
-  }
+  { id: "SH-001", name: "Diagonal Rainbow Sweep", price: "25", size: "MED", cat: "shirt", img: "shirt.jpeg", status: "available" },
+  { id: "PA-001", name: "Pastel Galaxy Joggers", price: "45", size: "LG", cat: "pants", img: "pants.jpeg", status: "available" },
+  { id: "TAP-001", name: "Electric Peace Mandala", price: "75", size: "Giant", cat: "tapestry", img: "tapestry.jpeg", status: "available" },
+  { id: "SK-001", name: "Watercolor Knee Highs", price: "10", size: "OS", cat: "socks", img: "socks.jpeg", status: "available" },
+  { id: "BD-001", name: "Sunburst Bedspread Set", price: "300", size: "Queen", cat: "bedding", img: "bedspread.jpeg", status: "available" }
 ];
