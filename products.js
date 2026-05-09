418c16d07d5648eeafe60f20daa66c665cc490af
sethcrisel22 <sethcrisel.22@gmail.com>
2026-05-09 10:05:55 -0700

    Update products.js

--- products.js
+++ products.js
@@ -17,7 +17,7 @@ const products = [
     price: "25", 
     size: "MED", 
     cat: "shirt", 
-    img: "https://i.imgur.com/f0d02B1.jpeg", 
+    img: "https://i.ibb.co/Q8M9Mmy/shirt.jpeg", 
     desc: "A classic crewneck tee featuring a vibrant, diagonal burst of color. Perfect for standing out.",
     status: "available" 
   },
@@ -27,7 +27,7 @@ const products = [
     price: "45", 
     size: "LG", 
     cat: "pants", 
-    img: "https://i.imgur.com/M3F2GAg.jpeg", 
+    img: "https://i.ibb.co/yQGq430/pants.jpeg", 
     desc: "Comfortable joggers with a soft, cosmic blend of pastel tones. Ideal for lounging or adventure.",
     status: "available" 
   },
@@ -37,7 +37,7 @@ const products = [
     price: "75", 
     size: "Giant", 
     cat: "tapestry", 
-    img: "https://i.imgur.com/kG2kR2y.jpeg", 
+    img: "https://i.ibb.co/yVv1VfK/tapestry.jpeg", 
     desc: "A massive, electrifying peace sign mandala. The ultimate statement piece for any wall or gathering.",
     status: "available" 
   },
@@ -47,7 +47,7 @@ const products = [
     price: "10", 
     size: "OS", 
     cat: "socks", 
-    img: "https://i.imgur.com/sC4xflb.jpeg", 
+    img: "https://i.ibb.co/hXf0xWp/socks.jpeg", 
     desc: "A subtle wash of watercolor hues on comfortable, durable knee-high socks. One size fits most.",
     status: "available" 
   },
@@ -57,7 +57,7 @@ const products = [
     price: "300", 
     size: "Queen", 
     cat: "bedding", 
-    img: "https://i.imgur.com/S965j7r.jpeg", 
+    img: "https://i.ibb.co/gZ1sMv9/bedspread.jpeg", 
     desc: "Transform your bedroom with this radiant sunburst design. Includes queen-sized duvet and shams.",
     status: "available" 
   }
